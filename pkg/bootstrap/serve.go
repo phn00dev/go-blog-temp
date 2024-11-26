b9841ecda6336528742254075c4d4c69381242a7
phn00dev <hudayberdipolat@gmail.com>
2024-11-26 10:08:54 +0500

    ready load static file package

--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -4,12 +4,15 @@ import (
 	"github.com/phn00dev/go-blog-temp/pkg/config"
 	"github.com/phn00dev/go-blog-temp/pkg/html"
 	"github.com/phn00dev/go-blog-temp/pkg/routing"
+	"github.com/phn00dev/go-blog-temp/pkg/static"
 )
 
 func Serve() {
 	config.Set()
 	routing.Init()
-	html.LoadHTML(routing.GetRouter())
+	getRouter := routing.GetRouter()
+	static.LoadStatic(getRouter)
+	html.LoadHTML(getRouter)
 	routing.RegisterRouter()
 	routing.Serve()
 }
